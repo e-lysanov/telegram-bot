1a837ce56ba6b32b42aa3a4c19b56b13ff650a77
e-lysanov <e.lysanov@yandex.ru>
2023-09-13 20:17:04 +0300

    4th commit: создал сущность NotificationTask

---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,66 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue
+    private Long p_key;
+    private Long chatId;
+    private String notification;
+    private LocalDateTime dateTime;
+
+    public NotificationTask() {
+    }
+
+    public Long getP_key() {
+        return p_key;
+    }
+
+    public void setP_key(Long p_key) {
+        this.p_key = p_key;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(p_key, that.p_key) && Objects.equals(chatId, that.chatId) && Objects.equals(notification, that.notification) && Objects.equals(dateTime, that.dateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(p_key, chatId, notification, dateTime);
+    }
+}
